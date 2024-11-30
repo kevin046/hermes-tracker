5114858810b2a9522c24396605c4da843d5aa9ee
kevin046 <85421994+kevin046@users.noreply.github.com>
2024-11-30 16:30:42 -0500

    Fix build and type issues

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,17 +1,20 @@
 import '@testing-library/cypress/add-commands'
 
-// Extend Cypress commands
 declare global {
   namespace Cypress {
     interface Chainable {
       login(email: string, password: string): Chainable<void>
+      findByLabelText(label: string | RegExp): Chainable<JQuery<HTMLElement>>
+      findByRole(role: string, options?: { name: string | RegExp }): Chainable<JQuery<HTMLElement>>
+      findByText(text: string | RegExp): Chainable<JQuery<HTMLElement>>
+      findByPlaceholderText(placeholder: string | RegExp): Chainable<JQuery<HTMLElement>>
     }
   }
 }
 
 Cypress.Commands.add('login', (email: string, password: string) => {
   cy.visit('/login')
-  cy.findByLabelText(/email/i).type(email)
-  cy.findByLabelText(/password/i).type(password)
-  cy.findByRole('button', { name: /login/i }).click()
+  cy.get('input[type="email"]').type(email)
+  cy.get('input[type="password"]').type(password)
+  cy.get('button[type="submit"]').click()
 }) 
